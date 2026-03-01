b788068ab232dd821761c082c7fcba45b771933e
charles <hxx34424@gmail.com>
2026-03-01 19:01:55 +0800

    feat: add timestamp to console log format

--- hedge/hedge_mode_bp_v2.py
+++ hedge/hedge_mode_bp_v2.py
@@ -91,7 +91,7 @@ class HedgeBot:
 
         # Create different formatters for file and console
         file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        console_formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+        console_formatter = logging.Formatter('%(asctime)s %(levelname)s:%(name)s:%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
         file_handler.setFormatter(file_formatter)
         console_handler.setFormatter(console_formatter)
